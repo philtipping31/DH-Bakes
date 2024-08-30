2bab7a6d23ee1a5f5ddd877d6d79d5b7d271a7a0
Phil Tipping <philtipping3110@gmail.com>
2024-08-30 08:47:12 +0000

    add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form':order_form,
+        'stripe_public_key': 'pk_test_51PfJpk2MEDs47a1gzMsUUH0LR4xLU72WuBCuGJQSz7CyGvvl2ofnIM6IGBAitG7JNmTEmQKEQSB0soFKQhnnysuN008G2P9NRX',
+        'client_secret': 'sk_test_51PfJpk2MEDs47a1glt3q7Idjim5VmzHlNYyaZi1D6PMU1AChjXgv8FGBNyaW92Pg2MCEE3sG0BzFGZZmI7sOIyx200Ldg5V3Dy',
     }
 
     return render(request, template, context)
