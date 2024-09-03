00d1859c91f6189f7a318223016cb2d1dcd1afc1
Phil Tipping <philtipping3110@gmail.com>
2024-09-03 14:50:26 +0000

    Updated check out forms added profile app

--- checkout/models.py
+++ checkout/models.py
@@ -1,8 +1,13 @@
 from decimal import Decimal
+
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 
@@ -11,7 +16,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     phone_number = models.CharField(max_length=20)
-    country = models.CharField(max_length=40)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, blank=True, null=True)
     town_or_city = models.CharField(max_length=40)
     street_address1 = models.CharField(max_length=80)
