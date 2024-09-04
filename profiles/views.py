11e31c12111bbb73b348885f55753f1851c6a526
Phil Tipping <philtipping3110@gmail.com>
2024-09-04 15:36:06 +0000

    Add order history, default info update and populate checkout details

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display the user's profile. """
@@ -27,3 +29,19 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past order confirmation for {order.number},'
+        'A confirmation email was sent to your email on the date of your order'
+    ))
+
+    template = checkout/checkout_success.html
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render (request, template, context)
