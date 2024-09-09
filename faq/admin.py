1c8cde17e05aeea9588372ae3082dacaf91cd78b
Phil Tipping <philtipping3110@gmail.com>
2024-09-09 15:17:10 +0000

    Add template for faq page, add url to nav, add faq admin

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import FAQ, FAQCategory
 
-# Register your models here.
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'category', 'order', 'is_active', 'created_at')
+    list_filter = ('is_active', 'category')
+    search_fields = ('question', 'answer')
+    ordering = ('order',)
+
+class FAQCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(FAQCategory, FAQCategoryAdmin)
