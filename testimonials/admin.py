fd78735bfeb2187534885bcddf74103471925a5a
Phil Tipping <philtipping3110@gmail.com>
2024-09-10 14:57:44 +0000

    Add testimonials to admin to add to site

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'rating', 'date_submitted', 'is_approved')
+    list_filter = ('is_approved', 'date_submitted')
+    search_fields = ('name', 'title', 'testimonial_text')
+    ordering = ('-date_submitted',)
+    actions = ['approve_testimonials']
+
+    def approve_testimonials(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_testimonials.short_description = 'Mark selected testimonials as approved'
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Testimonial
 
 
 # Create your views here.
