32666e716afa485ebc29c13321530f9b95fc1f49
Phil Tipping <philtipping3110@gmail.com>
2024-09-11 11:57:49 +0000

    Add recipe view, url and admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_posted', 'ingredients',
+                    'instructions', 'prep_time', 
+                    'cook_time', 'servings',
+                    'image', 'is_approved')
+
+    list_filter = ('is_approved', 'date_posted')
+
+    ordering = ('-date_posted',)
+
+    actions = ['approve_recipes']
+
+    # Custom admin action to approve selected recipes
+    def approve_recipes(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_recipes.short_description = 'Approve selected recipes'
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+
+def recipe_list(request):
+    """View to display a list of approved recipes"""
+
+    recipes = Recipe.objects.filter(is_approved=True).order_by('-date_posted')
+
+    template = 'recipes/recipe_list.html'
+    context = {
+        'recipes': recipes
+    }
+    return render(request, template, context)
