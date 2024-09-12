2adeb521b656118a41183395ee21ccdb474c805d
Phil Tipping <philtipping3110@gmail.com>
2024-09-12 14:30:23 +0000

    Add cache control

--- dh_bakes/settings.py
+++ dh_bakes/settings.py
@@ -191,8 +191,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket Config
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'dh-bakes'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
