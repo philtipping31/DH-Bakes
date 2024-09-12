f6e8617843479a28ebb2629c2ec5f5a4543238d2
Phil Tipping <philtipping3110@gmail.com>
2024-09-12 15:03:36 +0000

    Update project colour to comply with WAVE tests

--- dh_bakes/settings.py
+++ dh_bakes/settings.py
@@ -214,6 +214,7 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 
+
 #Delivery Amount
 STANDARD_DELIVERY = 5.0
 
